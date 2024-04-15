c3ff9e87a78a148eef35598cce701364c2b6cd50
DevDK12 <devdk2208@gmail.com>
2024-04-15 20:44:12 +0530

    Added new Products

--- src/error/catchAsync.ts
+++ src/error/catchAsync.ts
@@ -10,4 +10,18 @@ const CatchAsync = (
 }
 
 
+
+
+//_ DO this later
+
+// const CatchAsync = <T extends Request, U extends Response, V extends NextFunction> (
+//     fn: (req: T, res: U, next: V) => Promise<unknown>) => async (req: T, res: U, next: V) => {
+
+//     return Promise.resolve(fn(req, res, next))
+//     .catch(error => next(error));
+
+// }
+
+
+
 export default CatchAsync;
